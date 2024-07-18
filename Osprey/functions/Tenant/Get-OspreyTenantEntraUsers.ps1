f6396bbf9372fbd09fbad7d0494b280b6c92cf09
syne0 <syne0@pm.me>
2024-07-17 22:01:25 -0600

    Update per syne0/osprey#7

--- Osprey/functions/Tenant/Get-OspreyTenantEntraUsers.ps1
+++ Osprey/functions/Tenant/Get-OspreyTenantEntraUsers.ps1
@@ -1,18 +1,40 @@
 ﻿<#
 .DESCRIPTION
-    This function will export all the Azure Active Directory users to .csv file. This data can be used
-    as a reference for user presence and details about the user for additional context at a later time. This is a point
-    in time users enumeration. Date created can be of help when determining account creation date.
+    This function will export all the Azure Active Directory users to .csv file. It will also get all user creation
+    activity during the investigation period from the UAL.
 .OUTPUTS
     EntraIDUsers.csv
+    New_Users.csv
 #>
 Function Get-OspreyTenantEntraUsers {
     
     Out-LogFile "Gathering Entra ID Users"
     Send-AIEvent -Event "CmdRun"
 
-    Get-MgUser -all -Property UserPrincipalName, DisplayName, UserType, CreatedDateTime, AccountEnabled, Id, Mail, LastPasswordChangeDateTime | select-object UserPrincipalName, DisplayName, UserType, CreatedDateTime, AccountEnabled, Id, Mail, LastPasswordChangeDateTime | Out-MultipleFileType -fileprefix "EntraIDUsers" -csv
-    #TODO: If admin comp, mark list of users created during investigate period as needing further investigation.
-    #also more can be done with this probably.
+    #Obtaining all users in the tenant and outputting details to a csv
+    $TenantUsers = Get-MgUser -all -Property UserPrincipalName,DisplayName,UserType,CreatedDateTime,AccountEnabled,Id,Mail,LastPasswordChangeDateTime | select-object UserPrincipalName,DisplayName,UserType,CreatedDateTime,AccountEnabled,Id,Mail,LastPasswordChangeDateTime
+    $TenantUsers | Out-MultipleFileType -fileprefix "EntraIDUsers" -csv
+
     Out-Logfile "Completed exporting Entra ID users"
+
+    #Getting all user creation events from the investigation period and exporting to a CSV
+    $UserCreationLog = Get-AllUnifiedAuditLogEntry -UnifiedSearch ("Search-UnifiedAuditLog -Operation 'Add user.'")
+
+    if ($null -eq $UserCreationLog) {
+        Out-LogFile "No user creation activity found"
+    }
+    # If not null then we must have found some events so flag them
+    else {
+        foreach ($log in $UserCreationLog) {
+            $log1 = $log.auditdata | ConvertFrom-Json
+            $report = $log1 | Select-Object -Property CreationTime,
+            Id,
+            Operation,
+            UserID,
+            @{Name = 'User Added'; Expression = { ($_.Target[3].ID) } }
+
+            $report | Out-MultipleFileType -fileprefix "New_Users" -csv -append 
+        }
+    }
+
 }
