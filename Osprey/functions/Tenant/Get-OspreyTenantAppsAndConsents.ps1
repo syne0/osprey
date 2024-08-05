ec51d6c8784cb4db59bab859cb2ad71e317c3c0b
syne0 <syne0@pm.me>
2024-08-04 20:58:48 -0600

    Update per syne0/osprey#6

--- Osprey/functions/Tenant/Get-OspreyTenantAppsAndConsents.ps1
+++ Osprey/functions/Tenant/Get-OspreyTenantAppsAndConsents.ps1
@@ -6,10 +6,10 @@
 .OUTPUTS
     Entra_App_Consents.csv
     Entra_App_Consents.json
-    Suspicious_App_List.txt
+    Suspicious_App_List.csv
     Tenant_Applications.csv
     Tenant_Applications.json
-#> #conf 7/13
+#>
 Function Get-OspreyTenantAppsAndConsents {
     
     Test-EXOConnection
@@ -43,31 +43,47 @@ Function Get-OspreyTenantAppsAndConsents {
 
     ##Searching for known malicious applications##
 
-    $AllTenantApps = @(Get-MgServicePrincipal) #get display name of all apps in tenant
-    #TODO: use MS apps json i put on GH and exclude stuff inside of that list
+    $AllTenantApps = @(Get-MgServicePrincipal -all) #get all apps in tenant
     
     $SuspiciousApps = Invoke-RestMethod -URI https://raw.githubusercontent.com/randomaccess3/detections/main/M365_Oauth_Apps/MaliciousOauthAppDetections.json #pull list of malicious oauth apps from github
 
-    #TODO: ps custom object to get properties from both outputs
-    $MatchingApps = $($AllTenantApps | Where-Object displayname -in $SuspiciousApps.applications.name; $AllTenantApps | Where-Object appid -in $SuspiciousApps.appid) | Sort-Object appid -unique
+    $MatchingApps = $($AllTenantApps | Where-Object displayname -in $SuspiciousApps.applications.name; $AllTenantApps | Where-Object appid -in $SuspiciousApps.appid) | Sort-Object appid -unique #compare apps and record down any matches
 
     if ($null -eq $MatchingApps) {
-        Out-LogFile "No suspicious applications found in tenant"
+        Out-LogFile "No known suspicious applications found in tenant."
     }
     else {
         Out-LogFile "Suspicious application consents found within tenant! Please review applications." -notice
-        $MatchingApps | Out-MultipleFileType -FilePrefix "_Investigate_Suspicious_App_List" -txt -notice
-    }
 
-    <#
+        $AppOutput = foreach ($match in $MatchingApps) {
+            #do this for each app match we found
+
+            [PSCustomObject]@{ #build a custom object that takes information from various sources and outputs it
+                ApplicationName  = $match.DisplayName #from entra
+                ApplicationID    = $match.AppId #from entra
+                Enabled          = $match.AccountEnabled #from entra
+                Created          = $match.AdditionalProperties.createdDateTime #from entra
+                Description      = $SuspiciousApps.applications | Where-Object appid -match $match.AppId | Select-object -expandproperty description #from github list
+                UsersAssigned    = Get-MgServicePrincipalAppRoleAssignedTo -serviceprincipalid $match.id | Select-Object -expandproperty PrincipalDisplayname | Out-String #need to pull from additional cmd
+                Referances       = $SuspiciousApps.applications | Where-Object appid -match $match.AppId | Select-object -expandproperty references | Out-String  #from github list
+                KnownPermissions = $SuspiciousApps.applications | Where-Object appid -match $match.AppId | Select-object -expandproperty Permissions | Out-String #from github list
+                
+            }
+
+        }
+
+        $AppOutput | Out-MultipleFileType -FilePrefix "_Investigate_Suspicious_App_List" -csv -notice
+
+        <#
     ##Gathering all apps and their principles and permissions##
     # Using the script from the article https://docs.microsoft.com/en-us/microsoft-365/security/office-365-security/detect-and-remediate-illicit-consent-grants
 
     [array]$Grants = Get-AzureADPSPermissions -ShowProgress
     # [bool]$flag = $false
 
-    #This isnt super helpful since it always grabs some apps that are from Microsoft
+    #This isnt super helpful since it always grabs some apps that are from Microsoft and is noisy
     #TODO: Improve this bit to be more useful in situations of admin compromise
+    #clipping this out for now, will update after 1.0 is published
     
     # Search the Grants for the listed bad grants that we can detect
     if ($Grants.consenttype -contains 'AllPrinciples') {
@@ -91,4 +107,5 @@ Function Get-OspreyTenantAppsAndConsents {
 
     $Grants | Out-MultipleFileType -FilePrefix "Tenant_Applications" -csv -json
 #>
+    }
 }
