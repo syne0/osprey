da1e21da28229030b695ae94181da74d116091d8
syne0 <syne0@pm.me>
2024-08-11 14:31:29 -0600

    Updated per syne0/osprey#5

--- Osprey/functions/Tenant/Unused/Temporary_To_Fix/Search-OspreyTenantActivityByIP.ps1
+++ Osprey/functions/Tenant/Unused/Temporary_To_Fix/Search-OspreyTenantActivityByIP.ps1
@@ -1,5 +1,5 @@
 ﻿Function Search-OspreyTenantActivityByIP {
-<#
+    <#
 .SYNOPSIS
     Gathers logon activity based on a submitted IP Address.
 .DESCRIPTION
@@ -8,37 +8,14 @@
 .PARAMETER IPaddress
     IP address to investigate
 .OUTPUTS
-
-    File: All_Events.csv
-    Path: \<IP>
-    Description: All logon events
-
-    File: All_Events.xml
-    Path: \<IP>\xml
-    Description: Client XML of all logon events
-
-    File: Success_Events.csv
-    Path: \<IP>
-    Description: All logon events that were successful
-
-    File: Unique_Users_Attempted.csv
-    Path: \<IP>
-    Description: List of Unique users that this IP tried to log into
-
-    File: Unique_Users_Success.csv
-    Path: \<IP>
-    Description: Unique Users that this IP succesfully logged into
-
-    File: Unique_Users_Success.xml
-    Path: \<IP>\XML
-    Description: Client XML of unique users the IP logged into
+    All_Events.csv \ All_Events.xml \ All_Events.json
+    Login_Success_Events.csv \ Login_Success_Events.xml \ Login_Success_Events.json
+    Login_Failure_Events.csv \ Login_Failure_Events.xml \ Login_Failure_Events.json
+    Unique_Users_Login.csv \ Unique_Users_Login.xml \ Unique_Users_Login.json
 .EXAMPLE
-
     Search-OspreyTenantActivityByIP -IPAddress 10.234.20.12
-
     Searches for all Logon activity from IP 10.234.20.12.
 #>
-#TODO: Validate if this works, fix if not, and improve if possible
     param
     (
         [parameter(Mandatory = $true)]
@@ -57,42 +34,49 @@
         Write-Error -Message "Please provide a single IP address to search." -ErrorAction Stop
     }
 
-    Out-LogFile ("Searching for events related to " + $IpAddress) -action
+    Out-LogFile ("Searching for login events related to " + $IpAddress) -action
 
-    # Gather all of the events related to these IP addresses
-    [array]$ipevents = Get-AllUnifiedAuditLogEntry -UnifiedSearch ("Search-UnifiedAuditLog -IPAddresses " + $IPAddress )
+    ##Gather all of the events related to these IP addresses##
+    Out-LogFile ("Hold tight, this may take some time...")
+
+    [array]$ipevents = Get-AllUnifiedAuditLogEntry -UnifiedSearch ("Search-UnifiedAuditLog -RecordType AzureActiveDirectoryStsLogon -IPAddresses " + $IPAddress )
 
     # If we didn't get anything back log it
     if ($null -eq $ipevents) {
-        Out-LogFile ("No IP logon events found for IP "	+ $IpAddress)
+        Out-LogFile ("No IP login events found for IP "	+ $IpAddress)
     }
-
     # If we did then process it
     else {
 
-        # Expand out the Data and convert from JSON
+        ##Expand out the Data and convert from JSON##
         [array]$ipeventsexpanded = $ipevents | Select-object -ExpandProperty AuditData | ConvertFrom-Json
         Out-LogFile ("Found " + $ipeventsexpanded.count + " related to provided IP" )
-        $ipeventsexpanded | Out-MultipleFileType -FilePrefix "All_Events" -csv -json -User $DirectoryName
+        $ipeventsexpanded | Out-MultipleFileType -FilePrefix "All_Login_Events" -csv -json -xml -User $DirectoryName
 
-        # Get the logon events that were a success
-        [array]$successipevents = $ipeventsexpanded | Where-Object { $_.ResultStatus -eq "success" }
-        Out-LogFile ("Found " + $successipevents.Count + " Successful logons related to provided IP")
-        $successipevents | Out-MultipleFileType -FilePrefix "Success_Events" -csv -json -User $DirectoryName
+        ##Get the logon events that were a success##
+        [array]$successipevents = $ipeventsexpanded | Where-Object { $_.Operation -eq "UserLoggedIn" }
+        if ($null -eq $successipevents) {
+            Out-LogFile ("No successful logon events found for IP "	+ $IpAddress)
+        }
+        else {
+            Out-LogFile ("Found " + $successipevents.Count + " successful logons related to provided IP")
+            $successipevents | Out-MultipleFileType -FilePrefix "Login_Success_Events" -csv -json -xml -User $DirectoryName
+        }
+
+        ##Get the logon events that were a failure##
+        [array]$failedipevents = $ipeventsexpanded | Where-Object { $_.Operation -eq "UserLoginFailed" }
+        if ($null -eq $successipevents) {
+            Out-LogFile ("No failed logon events found for IP "	+ $IpAddress)
+        }
+        else {
+            Out-LogFile ("Found " + $failedipevents.Count + " failed logons related to provided IP")
+            $failedipevents | Out-MultipleFileType -FilePrefix "Login_Failure_Events" -csv -json -xml -User $DirectoryName
+        }
 
         # Select all unique users accessed by this IP
         [array]$uniqueuserlogons = Select-UniqueObject -ObjectArray $ipeventsexpanded -Property "UserID"
         Out-LogFile ("IP " + $ipaddress + " has tried to access " + $uniqueuserlogons.count + " users") -notice
-        $uniqueuserlogons | Out-MultipleFileType -FilePrefix "Unique_Users_Attempted" -csv -json -User $DirectoryName -Notice
-
-        if ($null -eq $uniqueuserlogonssuccess) {
-            Out-LogFile ("No Successful Logon Events found for this IP: " + $IpAddress)
-        }
-        else {
-            [array]$uniqueuserlogonssuccess = Select-UniqueObject -ObjectArray $successipevents -Property "UserID"
-            Out-LogFile ("IP " + $IpAddress + " SUCCESSFULLY accessed " + $uniqueuserlogonssuccess.count + " users") -notice
-            $uniqueuserlogonssuccess | Out-MultipleFileType -FilePrefix "Unique_Users_Success" -csv -json -User $DirectoryName -Notice
-        }
+        $uniqueuserlogons | Out-MultipleFileType -FilePrefix "Unique_Users_Login" -csv -json -User $DirectoryName -Notice
 
     }
 
