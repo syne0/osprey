4ebd16431eb462742818ff694330ce15cb142c9e
syne0 <syne0@pm.me>
2024-09-14 21:35:10 -0600

    Update per syne0/osprey#50

--- Osprey/functions/User/Get-OspreyUserConfiguration.ps1
+++ Osprey/functions/User/Get-OspreyUserConfiguration.ps1
@@ -5,12 +5,17 @@
 	* Get-EXOMailboxStatistics
 	* Get-EXOMailboxFolderStatistics
 	* Get-CASMailbox
+	Also gets autoreply and forwarding information.
 .OUTPUTS
 	Mailbox_Info.txt
 	Mailbox_Statistics.txt
 	Mailbox_Folder_Statistics.txt
 	CAS_Mailbox_Info.txt
-#>  #conf 7/13
+	AutoReply.txt
+	_Investigate_Users_WithForwarding.csv
+	User_ForwardingReport.csv
+	ForwardingReport.csv
+#>
 Function Get-OspreyUserConfiguration {
 
 	param
@@ -46,5 +51,38 @@ Function Get-OspreyUserConfiguration {
 		# Gather cas mailbox sessions
 		Out-LogFile "Gathering CAS Mailbox Information"
 		Get-EXOCasMailbox -identity $user | Out-MultipleFileType -FilePrefix "CAS_Mailbox_Info" -User $User -txt
+
+		##Autoreply##
+
+		# Get Autoreply Configuration
+		Out-LogFile ("Retrieving Autoreply Configuration: " + $User) -action
+		$AutoReply = Get-MailboxAutoReplyConfiguration -Identity  $User
+
+		# Check if the Autoreply is Disabled
+		if ($AutoReply.AutoReplyState -eq 'Disabled') {
+
+			Out-LogFile "AutoReply is not enabled or not configured."
+		}
+		# Output Enabled AutoReplyConfiguration to a generic txt
+		else {
+
+			$AutoReply | Out-MultipleFileType -FilePreFix "AutoReply" -User $user -txt
+		}
+
+		##forwarding##
+		
+		# Looking for email forwarding stored in AD
+		Out-LogFile ("Gathering possible Forwarding changes for: " + $User) -action
+		$mbx = Get-Mailbox -identity $User
+
+		# Check if forwarding is configured by user or admin
+		if ([string]::IsNullOrEmpty($mbx.ForwardingSMTPAddress) -and [string]::IsNullOrEmpty($mbx.ForwardingAddress)) {
+			Out-LogFile "No forwarding configuration found"
+		}
+		# If populated report it and add to a CSV file of positive finds
+		else {
+			Out-LogFile ("Found Email forwarding User:" + $mbx.primarySMTPAddress + " ForwardingSMTPAddress:" + $mbx.ForwardingSMTPAddress + " ForwardingAddress:" + $mbx.ForwardingAddress) -notice
+			$mbx | Select-Object DisplayName, UserPrincipalName, PrimarySMTPAddress, ForwardingSMTPAddress, ForwardingAddress, DeliverToMailboxAndForward, WhenChangedUTC | Out-MultipleFileType -FilePreFix "_Investigate_Users_WithForwarding" -append -csv -json -notice
+		}
 	}
 }
